5f429621b6b97f67cc851c6c2eb49d20655c1dae
Abner Silva <abner.js05@gmail.com>
2021-06-09 13:14:08 -0300

    date section selection UI final

---
+++ src/components/DateSectionController.jsx
@@ -0,0 +1,30 @@
+/* eslint-disable import/no-anonymous-default-export */
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import './DateSectionController.css'
+
+export default props => {
+    const [dateSelect, setDateSelect] = useState('Diário')
+
+    return (
+        <div className='date-section'>
+            <Link to='/home' onClick={() => setDateSelect('Diário')} className={`link
+                ${dateSelect === 'Diário' ? 'active' : ''}
+            `}>
+                Diário
+            </Link>
+            
+            <Link to='/home' onClick={() => setDateSelect('Semanal')} className={`link
+                ${dateSelect === 'Semanal' ? 'active' : ''}
+            `}>
+                Semanal
+            </Link>
+            <Link to='/home' onClick={() => setDateSelect('Mensal')} className={`link
+                ${dateSelect === 'Mensal' ? 'active' : ''}
+            `}>
+                Mensal
+            </Link>
+
+        </div>
+    )
+}
--- src/view/Details.jsx
+++ src/view/Details.jsx
@@ -1,13 +1,14 @@
 /* eslint-disable import/no-anonymous-default-export */
 import React from 'react'
 import DetailsTitle from '../components/DetailsTitle'
+import DateSectionController from '../components/DateSectionController'
 import './Details.css'
 
 export default props => {
     return (
         <div className='details'>
             <DetailsTitle name='Victor Rocha Azevedo' status='Online' />
-            
+            <DateSectionController />
         </div>  
     )
 }
