2e00ff6534ae3b84f4895408c1470b38ff9100b2
Abner Silva <abner.js05@gmail.com>
2021-06-10 17:13:12 -0300

    timeline fake data init

--- src/fakeData/DatetimeInfo.jsx
+++ src/fakeData/DatetimeInfo.jsx
@@ -1,6 +1,14 @@
-export const timeline = {
-    
-}
+const timeline = []
+const tmpaux = "2021-06-10 13:12:01"
+const tmpaux2 = "2021-06-10 15:15:16"
+timeline.push({
+    app: 'Google Chrome',
+    initialTime: new Date(Date.UTC(tmpaux[0], tmpaux[1]-1, tmpaux[2],
+        tmpaux[3], tmpaux[4], tmpaux[5])),
+    finalTime: new Date(Date.UTC(tmpaux2[0], tmpaux2[1]-1, tmpaux2[2],
+        tmpaux2[3], tmpaux2[4], tmpaux2[5])),
+    isAuth: true
+})
 
 export const pessoa = {
     nome: 'Victor Rocha Azevedo',
