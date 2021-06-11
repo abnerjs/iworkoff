8799e8dc3a3c678de74c5d308b98acca22f95b2c
Abner Silva <abner.js05@gmail.com>
2021-06-11 11:35:26 -0300

    chart init

--- src/components/dataInfo/ChartContainer.jsx
+++ src/components/dataInfo/ChartContainer.jsx
@@ -1,11 +1,31 @@
 /* eslint-disable import/no-anonymous-default-export */
 import React from 'react'
 import './ChartContainer.css'
+import Chart from "react-google-charts";
 
 export default props => {
     return (
         <div className='chart-container'>
-            
+            <Chart
+                width={'500px'}
+                height={'300px'}
+                chartType="Timeline"
+                loader={<div>Loading Chart</div>}
+                data={[
+                    [
+                        { type: 'string', id: 'President' },
+                        { type: 'date', id: 'Start' },
+                        { type: 'date', id: 'End' },
+                    ],
+                    ['Washington', new Date(1789, 3, 30), new Date(1797, 2, 4)],
+                    ['Adams', new Date(1797, 2, 4), new Date(1801, 2, 4)],
+                    ['Jefferson', new Date(1801, 2, 4), new Date(1809, 2, 4)],
+                ]}
+                options={{
+                    showRowNumber: true,
+                }}
+                rootProps={{ 'data-testid': '1' }}
+            />
         </div>
     )
 }
