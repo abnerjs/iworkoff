d7bc93253778f0ce9ca778623f30bdd40f86ee95
Abner Silva <abner.js05@gmail.com>
2021-06-14 08:59:44 -0300

    line height

--- src/components/dataInfo/ChartContainer.jsx
+++ src/components/dataInfo/ChartContainer.jsx
@@ -23,21 +23,32 @@ export default props => {
 
     return (
         <div className='chart-container'>
-            {/*
-            
             <Chart
-                width='100%'
-                height="500px"
+                width={'100%'}
+                height={'100%'}
                 chartType="Timeline"
-                loader={<div>Carregando chart</div>}
+                loader={<div className='loadingChart'>Carregando gráfico...</div>}
                 data={chartData}
+                render={({ renderChart }) => {
+                    return (
+                        <div style={{ display: 'flex', height: '100%' }}>
+                            <div style={{ width: '100%', height: '100%' }}>{renderChart()}</div>
+                        </div>
+                    )
+                }}
                 options={{
+                    colors: ['#009444', '#FBB040', '#BE1E2D', '#000'],
                     showRowNumber: true,
+                    timeline: {
+                        rowLabelStyle: {
+                            fontName: 'Poppins',
+                            fontSize: 14,
+                        },
+                        barLabelStyle: { fontName: 'Poppins', fontSize: 20 },
+                    },
                 }}
                 rootProps={{ 'data-testid': '0' }}
             />
-            
-            */}
         </div>
     )
 }
