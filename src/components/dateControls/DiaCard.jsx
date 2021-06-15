116a8d363b8f504b74186f4fd2afaf62082579d3
Abner Silva <abner.js05@gmail.com>
2021-06-15 13:34:46 -0300

    date select final carousel

--- src/components/dateControls/DiaCard.jsx
+++ src/components/dateControls/DiaCard.jsx
@@ -4,7 +4,7 @@ import './DiaCard.css'
 
 export default props => {
     return (
-        <div className={`dia-card ${props.active ? 'active':''}`}>
+        <div className={`dia-card ${props.active ? 'active':''}`} onClick={() => props.datesAround(props.delta)}>
             <div className="sem">{props.sem}</div>
             <div className="dia">{props.dia}</div>
             <div className="mes">{props.mes}</div>
--- src/components/dateControls/Diaria.jsx
+++ src/components/dateControls/Diaria.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable import/no-anonymous-default-export */
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import DiaCard from './DiaCard'
 import './Diaria.css'
 
@@ -7,51 +7,57 @@ export default props => {
     const [daySelect, setDaySelect] = useState(
         localStorage.getItem('daySelect') ? new Date(localStorage.getItem('daySelect')) : new Date()
     )
+
     useEffect(() => {
         localStorage.setItem('daySelect', daySelect)
-    }, [daySelect]);
+    }, [daySelect])
 
-    console.log(daySelect)
+    const datesAround = useCallback(function (delta) {
+        console.log(delta)
+        setDaySelect(new Date(daySelect.getTime() + (delta * 24 * 60 * 60 * 1000)))
+        console.log(daySelect)
+    }, [daySelect])
+
 
     const semana = ['DOM', 'SEG', 'TER', 'QUA', 'QUI', 'SEX', 'SÁB']
     const meses = ['JAN', 'FEV', 'MAR', 'ABR', 'MAI', 'JUN', 'JUL', 'AGO', 'SET', 'OUT', 'NOV', 'DEZ']
     return (
-        <div className='diaria'>
-            <DiaCard
+        <div className='diaria' >
+            <DiaCard datesAround={datesAround} delta={-3}
                 dia={(new Date(daySelect.getTime() - (3 * 24 * 60 * 60 * 1000))).getDate()}
                 mes={meses[new Date(daySelect.getTime() - (3 * 24 * 60 * 60 * 1000)).getMonth()]}
-                sem={semana[(props.timeline[0].initialTime.getDay() - 3 + 7) % 7]}
+                sem={semana[((daySelect.getDay() - 3) + 7) % 7]}
             />
-            <DiaCard 
+            <DiaCard datesAround={datesAround} delta={-2}
                 dia={(new Date(daySelect.getTime() - (2 * 24 * 60 * 60 * 1000))).getDate()}
                 mes={meses[new Date(daySelect.getTime() - (2 * 24 * 60 * 60 * 1000)).getMonth()]}
-                sem={semana[props.timeline[0].initialTime.getDay() - 2 + 7 % 7]} 
+                sem={semana[((daySelect.getDay() - 2) + 7) % 7]}
             />
-            <DiaCard 
+            <DiaCard datesAround={datesAround} delta={-1}
                 dia={(new Date(daySelect.getTime() - (1 * 24 * 60 * 60 * 1000))).getDate()}
                 mes={meses[new Date(daySelect.getTime() - (1 * 24 * 60 * 60 * 1000)).getMonth()]}
-                sem={semana[daySelect.getDay() - 1 + 7 % 7]} 
+                sem={semana[((daySelect.getDay() - 1) + 7) % 7]}
             />
-            <DiaCard 
+            <DiaCard datesAround={datesAround} delta={0}
                 active
                 dia={daySelect.getDate()}
-                mes={meses[daySelect.getMonth()]} 
-                sem={semana[daySelect.getDay()]} 
+                mes={meses[daySelect.getMonth()]}
+                sem={semana[daySelect.getDay()]}
             />
-            <DiaCard 
+            <DiaCard datesAround={datesAround} delta={1}
                 dia={(new Date(daySelect.getTime() + (1 * 24 * 60 * 60 * 1000))).getDate()}
                 mes={meses[new Date(daySelect.getTime() + (1 * 24 * 60 * 60 * 1000)).getMonth()]}
-                sem={semana[(props.timeline[0].initialTime.getDay() + 1) % 7]} 
+                sem={semana[(daySelect.getDay() + 1) % 7]}
             />
-            <DiaCard 
+            <DiaCard datesAround={datesAround} delta={2}
                 dia={(new Date(daySelect.getTime() + (2 * 24 * 60 * 60 * 1000))).getDate()}
                 mes={meses[new Date(daySelect.getTime() + (2 * 24 * 60 * 60 * 1000)).getMonth()]}
-                sem={semana[(props.timeline[0].initialTime.getDay() + 2) % 7]} 
+                sem={semana[(daySelect.getDay() + 2) % 7]}
             />
-            <DiaCard 
+            <DiaCard datesAround={datesAround} delta={3}
                 dia={(new Date(daySelect.getTime() + (3 * 24 * 60 * 60 * 1000))).getDate()}
                 mes={meses[new Date(daySelect.getTime() + (3 * 24 * 60 * 60 * 1000)).getMonth()]}
-                sem={semana[(props.timeline[0].initialTime.getDay() + 3) % 7]} 
+                sem={semana[(daySelect.getDay() + 3) % 7]}
             />
         </div>
     )
