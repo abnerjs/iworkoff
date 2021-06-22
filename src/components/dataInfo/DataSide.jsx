1e45ff55e31e9705c5fcc5926b80cc0fa304b6ab
Abner Silva <abner.js05@gmail.com>
2021-06-22 11:26:48 -0300

    media of data side ok

--- src/components/dataInfo/DataSide.jsx
+++ src/components/dataInfo/DataSide.jsx
@@ -8,14 +8,14 @@ function secondsToHours(sec) {
     var aux = new Date(1970, 0, 1)
     aux.setSeconds(sec)
     var textual = ''
-    
-    if(aux.getHours() > 0)
-        textual += aux.getHours() + 'h'
 
-    if(aux.getMinutes() > 0)
+    if (aux.getHours() > 0)
+        textual += (((aux.getDate() - 1) * 24) + aux.getHours()) + 'h'
+
+    if (aux.getMinutes() > 0)
         textual += aux.getMinutes() + 'min'
 
-    if(aux.getHours() <= 0 && aux.getMinutes() <= 0)
+    if (aux.getHours() <= 0 && aux.getMinutes() <= 0)
         textual += aux.getSeconds() + 's'
 
     return textual
@@ -32,21 +32,46 @@ function dateDisplay(dt) {
 }
 
 const DataSide = props => {
-    return (
-        <div className="dataside">
-            <Title content='Dados' />
-            <p>Hora de início: {dateDisplay(stringToDate(props.dtaHoraInicio))}</p>
-            <p>Hora final: {
-            (props.dtaHoraFim !== 'Ativo') ?
-                dateDisplay(stringToDate(props.dtaHoraFim)) :
-                'Ativo'
-            }</p>
-            <br />
-            <p>Tempo total: {secondsToHours(props.tempoRegistradoSegundos)}</p>
-            <p>Tempo ativo: {secondsToHours(props.totalAtiv)}</p>
-            <p>Tempo inativo: {secondsToHours(props.totalIdling)}</p>
-        </div>
-    )
+    if (props.typeOfAnalytics === 'Diário') {
+        return (
+            <div className="dataside">
+                <Title content='Dados' />
+                <p>Hora de início: {dateDisplay(stringToDate(props.dtaHoraInicio))}</p>
+                <p>Hora final: {
+                    (props.dtaHoraFim !== 'Ativo') ?
+                        dateDisplay(stringToDate(props.dtaHoraFim)) :
+                        'Ativo'
+                }</p>
+                <br />
+                <p>Tempo total: {secondsToHours(props.tempoRegistradoSegundos)}</p>
+                <p>Tempo ativo: {secondsToHours(props.totalAtiv)}</p>
+                <p>Tempo inativo: {secondsToHours(props.totalIdling)}</p>
+            </div>
+        )
+    } else if (props.typeOfAnalytics === 'Semanal') {
+
+        for(let dataDay of props.dataWeek) {
+            console.log(dataDay.tempoRegistradoSegundos)
+        }
+
+        return (
+            <div className="dataside">
+                <Title content='Dados' />
+                <p>Tempo total médio: {secondsToHours(props.dataWeek.reduce((a, b) =>
+                    a +
+                        b.tempoRegistradoSegundos || 0,
+                    0) / 5)}</p>
+                <p>Tempo ativo médio: {secondsToHours(props.dataWeek.reduce((a, b) =>
+                    a +
+                        b.tempoAtivoSegundos || 0,
+                    0) / 5)}</p>
+                <p>Tempo inativo médio: {secondsToHours(props.dataWeek.reduce((a, b) =>
+                    a +
+                        b.tempoInativoSegundos || 0,
+                    0) / 5)}</p>
+            </div>
+        )
+    }
 }
 
 const mapStateToProps = state => {
@@ -56,9 +81,11 @@ const mapStateToProps = state => {
         tempoRegistradoSegundos: state.timelineResult.tempoRegistradoSegundos,
         dtaHoraInicio: state.timelineResult.dtaHoraInicio,
         dtaHoraFim: state.timelineResult.dtaHoraFim,
+        typeOfAnalytics: state.timelineResult.typeOfAnalytics,
+        dataWeek: state.timelineResult.dataWeek,
     }
 }
 
 export default connect(
     mapStateToProps
-) (DataSide)
+)(DataSide)
